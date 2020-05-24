c4e467086971f0ba2c1048b89097574f3c712657
FKeir <fmkeir@gmail.com>
2020-05-24 02:16:35 +0100

    Add test: display error if user divides by zero

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -74,5 +74,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('0.25');
   });
 
+  it('Should tell the user it can\'t divide by zero if a number is divided by zero', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Can\'t divide by zero!');
+  });
 
 });
