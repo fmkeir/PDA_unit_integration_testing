64ee448df680c2946ea5724865c3d00829bc7874
FKeir <fmkeir@gmail.com>
2020-05-24 01:28:12 +0100

    Add test for addition

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -9,11 +9,10 @@ describe('calculator functionality', function() {
     browser.get('http://localhost:3000');
   });
 
-  // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  });
 
 });
--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -6,9 +6,14 @@ describe('calculator', function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
+
+  it('should be able to add two numbers', function(){
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    assert.equal(calculator.runningTotal, 5)
+  });
 
 });
